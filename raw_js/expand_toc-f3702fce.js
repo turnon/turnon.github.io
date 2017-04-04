4d9c3fa27e8bf9e80b4203a4e51259c476a6b885
ken <block24block@gmail.com>
2017-04-05 00:30:04 +0800

    update

--- raw_js/expand_toc-f3702fce.js
+++
@@ -1,6 +0,0 @@
-(function($){
-  $('.toc-button').on('click', function(e){
-    e.preventDefault();
-    $('nav').toggleClass('toc_expanded');
-  })
-})(jQuery);
--- raw_js/hovertag-71f2a4a7.js
+++ raw_js/hovertag-71f2a4a7.js
@@ -3,7 +3,7 @@
     var t=$(this);
     var wanted = loc.search.replace("?tag=","");
     if(wanted === t.find('span')[0].innerText){
-      t.addClass('hovertag');
+      t.parent().addClass('hovertag');
       return false;
     }
   });
