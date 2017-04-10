3f433c50175b6d87c097c4c17ab82749d08cdb44
ken <block24block@gmail.com>
2017-04-11 00:02:35 +0800

    fix bug: no hovered nav in post page

--- raw_js/hovernav-9a76ff38.js
+++ raw_js/hovernav-9a76ff38.js
@@ -1,6 +1,6 @@
 (function($){
   var nav = location.pathname;
-  if(nav !== '/'){
+  if(nav !== '/' && !nav.match(/\/archive/)){
     $('.current').removeClass('current');
     $('a[href="' + nav + '"]').closest('li').addClass('current');
   }
