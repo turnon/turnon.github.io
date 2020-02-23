671780cd269be29cf8df84d8c16eea711bacdf7a
ken <block24block@gmail.com>
2020-02-23 16:12:17 +0000

    update

--- raw_js/hovernav-9a76ff38.js
+++
@@ -1,7 +0,0 @@
-(function($){
-  var nav = location.pathname;
-  if(nav !== '/' && !nav.match(/\/archive/)){
-    $('.current').removeClass('current');
-    $('a[href="' + nav + '"]').closest('li').addClass('current');
-  }
-}(jQuery));
